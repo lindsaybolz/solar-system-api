626594a7fe2c38388a2c18c23af8320cbf98da62
Lindsay <lindsaybolzwilson@gmail.com>
2023-04-21 11:26:54 -0400

    built planets class, instances, blueprint and endpoint handle_planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, mass):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.mass = mass
+
+planets = [
+    Planet(1, 'Earth', 'Home planet', 5),
+    Planet(2, 'Mars', 'Fire planet', 4),
+    Planet(3, 'Dune', 'Home of the freemen', 9)
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planet_response = []
+    for planet in planets:
+        planet_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "mass": planet.mass
+        })
+    return jsonify(planet_response)
