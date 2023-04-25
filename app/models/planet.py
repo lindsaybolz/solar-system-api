e2e9a534e4ca5aad0e813be466feb49538588e5e
An Ba <ngwin.ab@gmail.com>
2023-04-25 11:20:46 -0400

    add models

---
+++ app/models/planet.py
@@ -0,0 +1,7 @@
+class Planet:
+    def __init__(self, id, name, description, mass):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.mass = mass
+
